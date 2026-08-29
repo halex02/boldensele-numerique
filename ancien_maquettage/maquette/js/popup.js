72b49e5ccb1d9bf29c9b936faec70cc38c4d5c84
Alexandre Moret <halex02@msn.com>
2026-08-29 20:18:38 +0200

    récupération du maquettage css que j’ai réalisé l’an dernier

---
+++ ancien_maquettage/maquette/js/popup.js
@@ -0,0 +1,18 @@
+let index = document.querySelectorAll('[data-index="index-nominum"]');
+
+let mouseOverHandler = function (e){
+    let identifier = e.nextSibling();
+    let popup = document.createElement("span");
+    popup.setAttribute("class" "popup");
+    e.insertAdjacentElement("afteren", popup)
+}
+
+let mouseOutHandler = function (e) {
+    e.
+}
+
+let mouseOutHandler = function ()
+foreach (let id in index){
+    id.addEventListener("mouseover", mouseOverHandler);
+    id.addEventListener("mouseout", mouseOutHandler);
+}
